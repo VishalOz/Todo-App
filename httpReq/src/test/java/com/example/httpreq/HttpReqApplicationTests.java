4bf60ac2f79a72d878c6d1c56e737e3094c33482
Vishal Sudasinghe <vishal.20240036@iit.ac.lk>
2025-08-08 08:11:21 +0530

    Initial commit - Spring Boot + React setup

---
+++ httpReq/src/test/java/com/example/httpreq/HttpReqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpreq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpReqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
